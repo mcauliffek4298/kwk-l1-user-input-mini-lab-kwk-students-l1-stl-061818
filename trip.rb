39b877e7fce27ecaebb94dc2c9468a8f26ab9a4f
mcauliffek4298 <kathryn@peerlift.org>
2018-06-18 20:24:40 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -3,6 +3,8 @@
 
 puts "Let's plan your trip! Where would you like to stay?"
 stay = gets.chomp.capitalize
-Where do you want to eat?
+puts "Where do you want to eat?"
 eat=gets.capitalize
+puts "How long do you want to stay?"
 nights=gets.capitalize
+puts "That sounds fun. You're staying at the #{stay}, and eating at the #{eat}, and you're staying for #{nights} long."
