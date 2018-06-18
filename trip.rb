229625473ecc9e030d469b26843f2751fc1733c6
mcauliffek4298 <kathryn@peerlift.org>
2018-06-18 20:15:07 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -13,4 +13,7 @@ stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts ""
+puts "Let's plan your trip! Where would you like to stay?"
+stay=gets.chomp.capitalize
+eat=gets.capitalize
+nights=gets.capitalize
